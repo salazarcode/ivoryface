bb310769e13b1ca184d969c2409d2bfbba6e8821
Your josemrs1995 <josemrs1995@gmail.com>
2018-05-30 10:43:40 -0400

    primer commit

---
+++ src/components/messages.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="messages">
+   <h1>Hola mundo</h1>
+   <p class="foter">esto es un footer</p>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'messages',
+  methods:{
+
+  }
+}
+</script>
+<style lang="scss">
+$blanco:#fff;
+.messages{
+    background: linear-gradient(45deg, rgba(77,166,254,1) 0%, rgba(0,228,253,1) 100%);
+    height: 264px;
+    border-radius: 15px;
+    box-shadow: 4px 4px 34px rgba(0, 0, 0, 0.2);
+}
+.foter{
+    background: transparent;
+    position: relative;
+    bottom: 0;
+    top:190px;
+    color: $blanco;
+    text-align: center;
+    border-top: dotted;
+    border-top-width:2px; 
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import layout from '@/components/layout'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'layout',
+      component: layout
+    }
+  ]
+})
